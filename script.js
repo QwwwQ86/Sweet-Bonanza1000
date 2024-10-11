29afb9a3e628bb68314977cadfcf2698e28e7437
QwwwQ86 <fedcenkosergej595@gmail.com>
2024-10-11 16:45:15 +0300

    Sweet-Bonanza1000
    Sweet-Bonanza1000

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector('.bonus-button').addEventListener('click', function() {
+    alert('Начать играть');
+});
+document.querySelector('.bonus-button2').addEventListener('click', function() {
+    alert('Получите бонус прямо сейчас!');
+});
+
